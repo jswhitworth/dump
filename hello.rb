2840b6871ad5f4b65c5356d759a007d7c0105ce1
Jeff Whitworth <jswhitworth@gmail.com>
2016-05-13 02:31:24 -0500

    moved rundeck, java, and apt-get update to default recipe

--- hello.rb
+++ hello.rb
@@ -1,27 +1,3 @@
 file '/tmp/motd' do
   content 'hello worl'
 end
-apt_update 'Update the apt cache daily' do
-  frequency 86_400
-  action :periodic
-end
-package 'openjdk-7-jdk'
-script 'extract_module' do
-  interpreter "bash"
-  code <<-EOH
-  export JAVA_HOME='/usr'
-  URL='http://dl.bintray.com/rundeck/rundeck-deb/rundeck-2.6.7-1-GA.deb'; FILE=`mktemp`; sudo wget "$URL" -qO $FILE && sudo dpkg -i $FILE; rm $FILE
-    EOH
-end
-file '/etc/rundeck/rundeck-config.properties' do
-  content '
-  loglevel.default=INFO
-  rdeck.base=/var/lib/rundeck
-  rss.enabled=false
-  dataSource.dbCreate = update
-  dataSource.url = jdbc:h2:file:/var/lib/rundeck/data/rundeckdb;MVCC=true;TRACE_LEVEL_FILE=4'
-  owner 'rundeck'
-  group 'rundeck'
-  mode '0640'
-  action :create
-end
